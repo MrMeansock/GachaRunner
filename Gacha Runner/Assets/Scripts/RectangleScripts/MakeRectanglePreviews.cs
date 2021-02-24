2c30c4f41bd473f94aaa400da574751148672e5d
crlimacastro <crlimacastro@gmail.com>
2021-02-24 13:56:43 -0500

    Finished separating Rectangle Maker and Previews Maker. Finished min length for rectangles

---
+++ Gacha Runner/Assets/Scripts/RectangleScripts/MakeRectanglePreviews.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+
+namespace GotchaGuys
+{
+    namespace GameRectangle
+    {
+        public class MakeRectanglePreviews : MonoBehaviour
+        {
+            [Header("Dependencies")]
+            [SerializeField] TouchHandler touchHandler;
+            [SerializeField] RectanglePreviewPool previewPool;
+
+            [Header("Parameters")]
+            [Tooltip("Minimum length a preview needs to be in order to create a rectangle.")]
+            [SerializeField] float minLength = 2.5f;
+            [Tooltip("Color of previews that are too short and won't make a rectangle.")]
+            [SerializeField] Color badPreviewColor = Color.gray;
+            [Tooltip("Color of previews long enough to make a rectangle.")]
+            [SerializeField] Color goodPreviewColor = Color.red;
+
+            // Tool logic variables
+            private RectanglePreview activePreview;
+
+            public event Action<RectanglePreview> OnGoodPreviewEnd;
+
+            private void Awake()
+            {
+                if (!touchHandler) touchHandler = FindObjectOfType<TouchHandler>();
+                if (!previewPool) previewPool = FindObjectOfType<RectanglePreviewPool>();
+            }
+
+            private void OnEnable()
+            {
+                touchHandler.OnFirstTouchStart += StartPreview;
+                touchHandler.OnFirstTouchMoved += UpdatePreview;
+                touchHandler.OnFirstTouchEnd += EndPreview;
+            }
+
+            private void OnDisable()
+            {
+                touchHandler.OnFirstTouchStart -= StartPreview;
+                touchHandler.OnFirstTouchMoved -= UpdatePreview;
+                touchHandler.OnFirstTouchEnd -= EndPreview;
+            }
+
+            void StartPreview(Touch touch)
+            {
+                activePreview = previewPool.GetNextAvailable(MainCamera.ScreenToWorldPoint(touch.position));
+                UpdateColor(activePreview);
+            }
+
+            void UpdatePreview(Touch touch, Vector2 deltaPosition)
+            {
+                activePreview.EndPosition = MainCamera.ScreenToWorldPoint(touch.position);
+                UpdateColor(activePreview);
+            }
+
+            void EndPreview(Touch touch)
+            {
+                activePreview.EndPosition = MainCamera.ScreenToWorldPoint(touch.position);
+                UpdateColor(activePreview);
+
+                // Invoke GoodPreview event if conditions met
+                if (activePreview.Length >= minLength)
+                {
+                    OnGoodPreviewEnd?.Invoke(activePreview);
+                }
+
+                previewPool.ReturnToPool(activePreview);
+                activePreview = null;
+            }
+
+            void UpdateColor(RectanglePreview preview)
+            {
+                if (preview.Length < minLength)
+                {
+                    preview.spriteRenderer.color = badPreviewColor;
+                }
+                else
+                {
+                    preview.spriteRenderer.color = goodPreviewColor;
+                }
+            }
+        }
+    }
+}
